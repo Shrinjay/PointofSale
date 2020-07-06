25366fcd61592fec46ff39672f8c2a5fa4d2baee
Shrinjay <shrinjaymukherjee@gmail.com>
2020-07-05 18:24:04 -0600

    Added line to server.js

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const mongoose = require('mongoose'); //Installs mongoose
 const cors = require('cors');  //Installs CORS
 require('dotenv').config(); //Installs dotenv, which I don't even use.
 
-
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const port = process.env.PORT || 3000 //This runs the server on port 3000, REVIEW SYNTAX.
 const uri = 'mongodb+srv://Shrinjay:sunny2002@shrinjay-rjqya.mongodb.net/<dbname>?retryWrites=true&w=majority' //URI to connect to mongoDB
