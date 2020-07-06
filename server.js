48ecc6fb736a80529e1dc8227fbc6fcb6e147730
Shrinjay <shrinjaymukherjee@gmail.com>
2020-07-05 19:54:47 -0600

    Added route

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
     const path = require('path');
     app.use(express.static('client/build'));
     app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, './client/build', 'index.html'));
       });
   
   }
