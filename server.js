1d4efe48f03078ae4316937cd95caa3adddaebe6
Shrinjay <shrinjaymukherjee@gmail.com>
2020-07-05 18:56:23 -0600

    Fixed?

--- server.js
+++ server.js
@@ -9,13 +9,8 @@ require('dotenv').config(); //Installs dotenv, which I don't even use.
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
     const path = require('path');
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
   
-    // Express serve up index.html file if it doesn't recognize route
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
   }
 
 const port = process.env.PORT || 3000 //This runs the server on port 3000, REVIEW SYNTAX.
